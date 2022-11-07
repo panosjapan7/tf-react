139669cfdfde1ac85c0015fbf5ad9a8271b681fa
panosjapan7 <panosjapan@gmail.com>
2022-11-07 15:17:56 +0100

    Styles main tag, creates img tag

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <main></main>
+      <main style={{width: "1600px", height: "1109px"}}>
+        <img src='./tiger-Siberian.jpg' />
+      </main>
     </div>
   );
 }
