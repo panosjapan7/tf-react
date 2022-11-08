c04f3c6d79a75f374687856538afd7dedaf4b8a9
panosjapan7 <panosjapan@gmail.com>
2022-11-08 08:29:28 +0100

    Adds header, padding

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{padding: "0 2rem"}}>
+      <header>
+        <h1>Nackademin Teknikfördjupning Projekt by Panagiotis Tsapanidis</h1>
+        <h2>React.js</h2>
+      </header>
       <main style={{width: "1600px", height: "1109px"}}>
-        <img src='./tiger-Siberian.jpg' />
+        <img src='./tiger-Siberian.jpg' alt="tiger"/>
       </main>
     </div>
   );
